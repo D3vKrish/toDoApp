cb92710912131bf08cb5bcd7a6127599dae9a3a8
Krish Garg <er.krishgarg@gmail.com>
2025-06-17 15:45:31 +0530

    update

--- src/controllers/passwordReset.controller.js
+++ src/controllers/passwordReset.controller.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const passwordResetController = async (req, res) => {
     try {
         const { userId, newPassword } = req.body;
-        // Input validation - ensures all fields are provided
+        // Ensure all fields are provided
         if (!userId || !newPassword) {
             return res.status(400).json({
                 status: 400,
@@ -14,7 +14,6 @@ const passwordResetController = async (req, res) => {
         }
         //Find user in database by userId
         const existingUser = await User.findOne({ userId: userId });
-        // Check if old password matches
         if (!existingUser) {
             return res.status(404).json({
                 status: 404,
@@ -36,6 +35,7 @@ const passwordResetController = async (req, res) => {
         // Hash the new password
         const salt = await bcrypt.genSalt(10);
         const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+        
         // Update user password
         existingUser.password = hashedNewPassword;
         // Save updated user record
